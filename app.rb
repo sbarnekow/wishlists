210c6da6d13dd2044d8e30e4edee89cd5f2e4026
Sarah Barnekow <mellamopepe@gmail.com>
2015-11-25 12:39:46 -0800

    everything working for non paginated baby registry IN THE US ONLY

--- app.rb
+++ app.rb
@@ -10,26 +10,44 @@ end
 
 post '/wishlist/' do
   @wishlist_url = params[:wishlist][:url]
-  @wishlist_detail = parse_wishlist(@wishlist_url)
-  puts @wishlist_url
-  puts @wishlist_detail
-  erb :wishlist, locals: {wishlist_data: @wishlist_detail}
+  @wishlist_items = get_wishlist_items(@wishlist_url)
+  @wishlist_deets = get_wishlist_details(@wishlist_url)
+  erb :wishlist, locals: { wishlist_items: @wishlist_items, wishlist_details: @wishlist_deets }
 end
 
-
-
-
-def parse_wishlist(wishlist_url)
+def get_wishlist_details(wishlist_url)
   doc = Nokogiri::HTML(open(wishlist_url))
-  wishlist_item_data = []
-  # doc.css('[id^=itemName]').each do |link|
-  #   puts link.content
-  # end
-  doc.css('[id^=item_]').each do |info|
-    name = info.css('[id^=itemName]').text.strip
-    price = info.css('[id^=itemPrice]').text.strip
-    image = info.css('[id^=itemImage] img')
-    a_tag = info.css('[id^=itemImage] a')
+  wishlist_details = {}
+  doc.css('[id^=reg-info]').each do |info|
+    wishlist_title = info.css('h1').text.strip
+    baby_deets = []
+    for nth_child in 1..5
+      detail = info.css("div.a-row.a-spacing-top-none.a-size-base.reg-meta-data > span:nth-child(#{nth_child})").text.strip
+      baby_deets.push(detail)
+    end
+    wishlist_details[:title] = wishlist_title
+    wishlist_details[:due_date] = baby_deets[1]
+    wishlist_details[:time_until] = baby_deets[2]
+    wishlist_details[:location] = baby_deets[4]
+  end
+  return wishlist_details
+end
+
+def get_wishlist_items(wishlist_url)
+  doc = Nokogiri::HTML(open(wishlist_url))
+  wishlist_items = []
+  doc.css('[id^=item_I]').each do |info|
+    # name = info.css('[id^=itemName]').text.strip
+    # price = info.css('[id^=itemPrice]').text.strip
+    # image = info.css('[id^=itemImage] img')
+    # a_tag = info.css('[id^=itemImage] a')
+    # link_val = a_tag.xpath('@href').first
+    # link = "https://amzn.com#{link_val}/?tag=blilis-20"
+
+    name = info.css('[id^=item_title]').text.strip
+    price = info.css('[id^=item_price]').text.strip
+    image = info.css('[id^=item-img] img')
+    a_tag = info.css('[id^=item-img] a')
     link_val = a_tag.xpath('@href').first
     link = "https://amzn.com#{link_val}/?tag=blilis-20"
 
@@ -43,12 +61,7 @@ def parse_wishlist(wishlist_url)
     fourhalf_rating = info.css('.a-star-4-5').text
     five_rating = info.css('.a-star-5').text
 
-    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&  rating #{fourhalf_rating}"
-    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&  rating #{four_rating}"
-    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&  rating #{five_rating}" 
-
-    wishlist_item_data.push({name: name, price: price, image: image, link: link})
+    wishlist_items.push({name: name, price: price, image: image, link: link})
   end
-
-  return wishlist_item_data
+  return wishlist_items
 end
